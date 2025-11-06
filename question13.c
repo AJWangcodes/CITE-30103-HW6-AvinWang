fe81cff2d032c2bff1813563328452b654ed6a6e
AJWangcodes <avinjwang@gmail.com>
2025-11-06 13:00:45 -0600

    Delete question13.c

--- question13.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(void){
-	int n;
-	printf("Enter a number: ");
-	scanf("%d", &n);
-	int ok = (n % 2 == 0) && (n % 3 == 0) && !(n % 7 == 0);
-	printf("Divisible by 2 and 3 but not 7? %s\n", ok ? "YES" : "NO");
-	return 0;
-}
